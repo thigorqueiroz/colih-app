84166894f303d6b1f2670750ce4e7772b568c185
thigorqueiroz <thigorqueiroz@gmail.com>
2021-08-29 16:34:10 -0300

    WIP

--- go.mod
+++ go.mod
@@ -6,6 +6,9 @@ require (
 	cloud.google.com/go/firestore v1.5.0 // indirect
 	cloud.google.com/go/storage v1.16.0 // indirect
 	firebase.google.com/go v3.13.0+incompatible // indirect
+	github.com/go-resty/resty/v2 v2.6.0 // indirect
+	github.com/zabawaba99/firego v0.0.0-20190331000051-3bcc4b6a4599 // indirect
 	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f // indirect
 	google.golang.org/api v0.54.0 // indirect
+	gopkg.in/zabawaba99/firego.v1 v1.0.0-20190331000051-3bcc4b6a4599 // indirect
 )
--- main.go
+++ main.go
@@ -1,44 +1,42 @@
 package main
 
 import (
-	"context"
-	firebase "firebase.google.com/go"
 	"fmt"
-	"google.golang.org/api/iterator"
-	"log"
+	"github.com/go-resty/resty/v2"
 )
 
 func main () {
 	println("Initializing COLIH app ...")
-	ctx := context.Background()
-	conf := &firebase.Config{
-		ProjectID: projectID
-	}
-	app, err := firebase.NewApp(ctx, conf)
-	if err != nil {
-		log.Fatalln(err)
-	}
+	client := resty.New()
 
-	client, err := app.Firestore(ctx)
-	if err != nil {
-		log.Fatalln(err)
-	}
+	resp, err := client.R().
+		EnableTrace().
+		Get("https://colih-recife.firebaseio.com/schedule.json")
 
-	defer client.Close()
+	// Explore response object
+	fmt.Println("Response Info:")
+	fmt.Println("  Error      :", err)
+	fmt.Println("  Status Code:", resp.StatusCode())
+	fmt.Println("  Status     :", resp.Status())
+	fmt.Println("  Proto      :", resp.Proto())
+	fmt.Println("  Time       :", resp.Time())
+	fmt.Println("  Received At:", resp.ReceivedAt())
+	fmt.Println("  Body       :\n", resp)
+	fmt.Println()
 
-
-	iter := client.Collection("PERGUNTAR A GUGA").Documents(ctx)
-	for {
-		doc, err := iter.Next()
-		if err == iterator.Done {
-			break
-		}
-
-		if err != nil {
-			log.Fatalf("Failed to iterate: %v", err)
-		}
-
-		fmt.Println(doc.Data())
-
-	}
+	// Explore trace info
+	fmt.Println("Request Trace Info:")
+	ti := resp.Request.TraceInfo()
+	fmt.Println("  DNSLookup     :", ti.DNSLookup)
+	fmt.Println("  ConnTime      :", ti.ConnTime)
+	fmt.Println("  TCPConnTime   :", ti.TCPConnTime)
+	fmt.Println("  TLSHandshake  :", ti.TLSHandshake)
+	fmt.Println("  ServerTime    :", ti.ServerTime)
+	fmt.Println("  ResponseTime  :", ti.ResponseTime)
+	fmt.Println("  TotalTime     :", ti.TotalTime)
+	fmt.Println("  IsConnReused  :", ti.IsConnReused)
+	fmt.Println("  IsConnWasIdle :", ti.IsConnWasIdle)
+	fmt.Println("  ConnIdleTime  :", ti.ConnIdleTime)
+	fmt.Println("  RequestAttempt:", ti.RequestAttempt)
+	fmt.Println("  RemoteAddr    :", ti.RemoteAddr.String())
 }
